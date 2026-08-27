6ea34e99a9780b2f9eb5ec739b4c96ed2cadd96c
Rayyan Shaheer <rayyanshaheer2@gmail.com>
2026-08-28 02:20:36 +0500

    feat: add tailwind css integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,12 @@
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+
 export default defineConfig({
   site: 'https://cloud-native-peshawar.github.io',
   base: '/cncf-peshawar-website',
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
